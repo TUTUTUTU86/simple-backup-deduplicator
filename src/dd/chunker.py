942660f4388d1eeda5fdd003f11b2e38a1eb5dcd
TUTUTUTU86 <100689186+TUTUTUTU86@users.noreply.github.com>
2022-03-09 16:45:05 +0300

    Have written chunk() method in chunker.py

--- src/dd/chunker.py
+++ src/dd/chunker.py
@@ -1,11 +1,14 @@
 from pathlib import Path
+from fastcdc import fastcdc_py
 
 
 class Chunker:
     def __init__(self):
         ...
 
-    def chunk(self, file: Path):
-        chunks = []
+    @staticmethod
+    def chunk(file: Path):
+        with file.open("rb") as stream:
+            chunks = fastcdc_py.fastcdc_py(stream)
         return chunks
 
