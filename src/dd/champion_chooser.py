0292983b70740626a8a2d354223ead13e0d9b27b
TUTUTUTU86 <100689186+TUTUTUTU86@users.noreply.github.com>
2022-04-01 17:30:29 +0300

    -finished champion chooser

--- src/dd/champion_chooser.py
+++ src/dd/champion_chooser.py
@@ -11,8 +11,32 @@ class ChampionChooser:
         self.index = {}
 
     def choose(self, hooks) -> list:
+        hooks = set(hooks)
         champions = []
+        while len(champions) < self.max_champions:
+            segments_score = {}
+            segments_hooks = {}
+            for hook in hooks:
+                if hook in self.sparse_index.sparse_index:
+                    for segment_id in self.sparse_index.get(hook):
+                        if segment_id in segments_score:
+                            segments_score[segment_id] += 1
+                            segments_hooks[segment_id] = set([hook])
+                        else:
+                            segments_score[segment_id] = 1
+                            segments_hooks[segment_id].update(set([hook]))
 
+            max_score = 0
+            for segment in segments_score:
+                if segments_score[segment] > max_score:
+                    max_score = segments_score[segment]
+                    champion = segment
+            if max_score != 0:
+                hooks -= segments_hooks[champion]
+                champions.append(champion)
+            else:
+                break
+        return champions
 
     def add(self, manifest: Manifest, hooks):
         segment_id = self.manifest_store.counter
